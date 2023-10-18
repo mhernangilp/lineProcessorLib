b097b7c3da06d21e87789658bc8c7c4418d45120
mhernangilp <mhernangilp@gmail.com>
2023-10-18 15:17:15 +0200

    moved macros from libreria.h to test.c

--- libreria.h
+++ libreria.h
@@ -5,10 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ERR_ARGS "Usage: test -<function> [numero de lineas]"
-#define ERR_N "[numero de lineas] not valid"
-#define ERR_FUNC "<function> not valid: '-head', '-tail' or '-longlines'"
-
 int	head(int N);
 int	tail(int N);
 int	longlines(int N);
--- test.c
+++ test.c
@@ -1,4 +1,7 @@
 #include "libreria.h"
+#define ERR_ARGS "Usage: test -<function> [numero de lineas]"
+#define ERR_N "[numero de lineas] not valid"
+#define ERR_FUNC "<function> not valid: '-head', '-tail' or '-longlines'"
 
 static void	exit_msg(char *msg)
 {
