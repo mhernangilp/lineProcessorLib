0d873cf0396a38f42e9fb8e28463ad077af64b1f
mhernangilp <mhernangilp@gmail.com>
2023-11-03 13:26:30 +0100

    added flags

--- compila.sh
+++ compila.sh
@@ -3,7 +3,7 @@
 #Compilación
 gcc -c libreria.c -o libreria.o
 ar -rv liblibreria.a libreria.o
-gcc -o test test.c -L ./ -llibreria #-g3 -fsanitize=address
+gcc -o test test.c -L ./ -llibreria -Wall -Werror -Wextra #-g3 -fsanitize=address
 
 #Limpieza de objetos
 rm -rf *.a *.o
