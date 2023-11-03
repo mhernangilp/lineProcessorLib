2fd5acc5cebf29a71868b825b44b7ae52d0e9394
mhernangilp <mhernangilp@gmail.com>
2023-11-03 14:01:55 +0100

    fixed compila.sh

--- compila.sh
+++ compila.sh
@@ -3,8 +3,8 @@
 #Compilación
 gcc -c libreria.c -o libreria.o
 ar -rv liblibreria.a libreria.o
-gcc -o test test.c -L ./ -llibreria -Wall -Werror -Wextra #-g3 -fsanitize=address
+gcc -o test test.c -I . -L . -llibreria -Wall -Werror -Wextra #-g3 -fsanitize=address
 
 #Limpieza de objetos
-rm -rf *.a *.o
+rm -rf *.o
 echo -e "\e[1;32mCompilation finished !\e[0m"
