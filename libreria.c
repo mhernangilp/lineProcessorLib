0658af66630b5c7be861bc19456fddd89c0218d1
josrob13~ <jose13.villagran@gmail.com>
2023-11-13 15:59:07 +0100

    split_lines done

--- libreria.c
+++ libreria.c
@@ -19,7 +19,7 @@ int	tail(int N)
 	if (!(input = read_input()))
 		return (-1);
 	printf("%s", input);
-	if (!(lines = split_lines(input)))
+	if (!(lines = split_lines("ola marcos\nesta es una\nprueba para\n ver quetal\n")))
 		return (-1);
 	free(input);
 	return (0);
@@ -32,9 +32,81 @@ int	longlines(int N)
 }
 
 //Splits the input in multiple strings separated by '\n', terminating in NULL
-static char	**split_lines(char *input)
+static char **split_lines(char *input)
 {
-	return (NULL);
+    char    **lines;
+    char    *buf, *token, *p;
+    char    *delimit = "\n";
+    int     num_lineas, i;
+
+    if (!(buf = strdup(input)))
+    {
+        free(buf);
+        return (NULL);
+    }
+    // buf = input;
+
+    // Para contar el numero de lineas que tiene el input (el buf)
+    while (*buf) {
+        if (*buf == '\n') {
+            num_lineas++;
+        }
+        buf++;
+    }
+
+    if (!(p = strdup(input)))
+    {
+        free(p);
+        return (NULL);
+    }
+
+    // Declaro token, que guardara las distintas lineas de la entrada y las ira pasando a lines
+    token = strtok(p, delimit);
+    if (!token)
+    {
+        free(token);
+        return(NULL);
+    }
+    num_lineas = 1;
+    i = 0;
+
+    // Reservo memoria para lines (para los primeros huecos de puntero)
+    if(!(lines = (char **)calloc((num_lineas + 1), sizeof(char *)))) {
+        free(lines);
+        free(token);
+        return (NULL);
+    }
+
+    // Mientras exista token, hago una copia de token y la asigno al espacio de memoria reservado de lines correspondiente
+    while (token)
+    {
+        //printf("TOKEN %d: '%s'", i, token);
+        if(!(lines[i] = strdup(token))) {
+            for (int j = 0; j < i; j++) {
+                free(lines[j]);
+            }
+            free(buf);
+            return(NULL);
+        }
+        printf("LINES[%d]: '%s'", i, lines[i]);
+
+        token = strtok(NULL, delimit);
+        i++;
+    }
+	
+	//printf("LINEA 1: %s, LINEA 2: %s, LINEA 3: %s", lines[0], lines[1], lines[2]);
+
+    printf("\n------------------------------\n");
+    for (int k = 0; k < i; k++)
+    {
+        printf("Linea[%d]: %s", k, lines[k]);
+    }
+    
+    printf("\n------------------------------\n");
+
+    lines[i] = NULL;
+    free(token);
+    return (lines);
 }
 //Reads the input using a temporal buffer, returns a string with the input
 static char	*read_input()
